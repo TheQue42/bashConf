4d315b42d6d1768043f684281f22e39cdf896bfb
Taisto Lx-Raistlin Qvist <taisto.qvist@gmail.com>
2021-08-05 21:58:41 +0200

    updated-extra-copy-of-checkbasconf

--- checkBashConf.sh
+++ checkBashConf.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/env bash
 cd ~
-if [[ $1 =~ -a|--all ]]
+if [[ $1 =~ -a$|--all$ ]]
 then
     IDENTICAL=YES
     shift
 fi
+
+function isSameContents()
+{
+	if diff -q $1 $2 >/dev/null 2>&1 
+	then
+		#cecho "$1 diff's the same as $2"
+		export DATE_SYNC="NEEDED"
+		SHA1=$(sha1sum $1 | cut -d" " -f 1)
+		SHA2=$(sha1sum $2 | cut -d" " -f 1)
+		if ! [ "$SHA1" == "$SHA2" ]
+		then
+			cecho -bold red "diff-check and sha dont agreee on $1:$SHA1 and $2:$SHA2"
+			return -2
+		fi	
+		return 0
+	fi
+	return 1
+}
 
 cd ~/bashConf
 echo "Fetching latest git updates from repo..."
@@ -27,7 +45,14 @@ do
     then
         NEWER="${RED}newer$CYAN"
         OLDER="${RED}older$CYAN"
-        if [ $CURR -nt $GIT ]
+        if isSameContents $CURR $GIT
+        then
+			if [ $GIT -nt $CURR -o $CURR -nt $GIT ]
+			then
+				cecho -bold "We need timesync for $CURR:"
+				rsync -a $GIT $CURR
+			fi
+        elif [ $CURR -nt $GIT ]
         then
             echo ""
             cecho -bold "$CURR is $NEWER than $GITFILE"
@@ -36,6 +61,7 @@ do
             cecho -bold green "rsync -avcu $CURR $GIT"
             echo ""
             NODIFFS=FALSE
+            
         elif [ $GIT -nt $CURR ]
         then
             echo ""
@@ -44,9 +70,10 @@ do
             cecho green "kdiff3 $GIT $CURR"
             cecho -bold green "rsync -avcu $GIT $CURR"
             echo ""
-            NODIFFS=FALSE            
+            NODIFFS=FALSE
         elif [ "$IDENTICAL" == "YES" ]
         then
+            ### TQ_TODO: Bugg in CLI param
             cecho green "File $CURR seems to be ${WHITE}identical$GREEN to $GIT"
         fi
     elif [ -s $CURR -o -s GIT ]
@@ -60,9 +87,10 @@ done
 if [ $NODIFFS == "TRUE" ]
 then
     cd ~/bashConf
+    COMMIT_MESSAGE="${*:-Misc updates}"
     if git status | egrep "(Changes to be committed|Changes not staged for commit)"
     then
-        cecho "cd ~/bashConf; git add -u ; git commit -m misc; git push"
+        cecho "cd ~/bashConf; git add -u ; git commit -m \"$COMMIT_MESSAGE\"; git push"
     fi
 fi
 
